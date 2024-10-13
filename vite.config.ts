c7329469f4cefd610248f0aa8eff4a4509d8ce55
enunez <enunez.dev@gmail.com>
2024-10-12 23:39:18 -0400

    udpate yml

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Configura el puerto aquí
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts']
-  }
+    setupFiles: ['./src/setupTests.ts'],
+  },
 });
